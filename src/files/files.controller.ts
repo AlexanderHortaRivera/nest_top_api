45c6d43164c284f9270409352084733470090ace
Alexander Horta <alexhortariv@gmail.com>
2022-05-03 12:00:20 +0700

    Работа с файлами (конвертация SHARP)

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { FileElementResponse } from "./dto/file-element.response";
 import { FilesService } from "./files.service";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
+import { MFile } from "./mfile.class";
 
 @Controller('files')
 export class FilesController {
@@ -14,7 +15,18 @@ export class FilesController {
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('files'))
 	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
-		return this.filesService.saveFiles([file]);
+
+		const saveArray: MFile[] = [new MFile(file)];
+
+		if(file.mimetype.includes('image')) {
+			const buffer = await this.filesService.convertToWebP(file.buffer);
+			saveArray.push(new MFile({
+				originalname: `${file.originalname.split('.')[0]}.webp`,
+				buffer
+			}));
+		}
+
+		return this.filesService.saveFiles(saveArray);
 	}
 
 
